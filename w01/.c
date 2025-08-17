8b66c6b3123536cde79022e07707fee6fa518998
Sahil Hareshbhai Patel <sahilpatel31082004@gmail.com>
2025-08-17 03:40:43 -0400

    Add files via upload

---
+++ w01/.c
@@ -0,0 +1,19 @@
+/*
+Full Name : Sahil Hareshbhai Patel
+Student ID# : 173793225
+UserID / Email : shpatel27 | shpatel27@myseneca.ca
+*/
+
+#include <stdio.h> // header file with Input/Output functions
+int main(void) // main() is automatically called to start a C program
+{
+	printf("Workshop 1 Part-1\n");
+	printf("=================\n\n");
+	printf("I'm displaying this using the 'printf' stdio\n");
+	printf("(standard input output) library function!\n\n");
+	printf("Dear teacher,\n\n  I promise I will work hard from this day onward.\n  I acknowledge that practice is extremely important,\n  so I will do all workshops, quizzes, and assignments.\n\n");
+	printf("Sincerely,\n\n");
+	printf("Sahil Hareshbhai Patel\n");
+	printf("Student ID# 173793225");
+	return 0;   // normal end of program
+}
---
+++ w01/w1p2.c
@@ -0,0 +1,26 @@
+/*
+Full Name : Sahil Hareshbhai Patel
+Student ID# : 173793225
+UserID / Email : shpatel27 | shpatel27@myseneca.ca
+*/
+#include <stdio.h> // header file with Input/Output functions
+int main(void) // main() is automatically called to start a C program
+{
+	printf("Workshop 1 Part-2\n");
+	printf("=================\n\n");
+	printf("Using tab specifiers...\n");
+	printf("\tTab-1\tTab-2\tTab-3\n");
+	printf("\t-----\t=====\t-----\n\n");
+	printf("\\ this is a back-slash character!\n");
+	printf("%% this is a percent sign character!\n");
+	printf("\" this is a double-quote character!\n\n");
+	printf("My favourite quotes are:\n");
+	printf("1.\t\"Skill is only developed by hours and hours of work.\"\n");
+	printf("\t\t\t\t\t\t-Usain Bolt\n\n");
+	printf("2.\t\"It's not about having time. It's about making time.\"\n");
+	printf("\t\t\t\t\t\t-unknown\n\n");
+	printf("3.\t\"All of us do not have equal talent. But, all of us\n");
+	printf("\thave an equal opportunity to develop our talents.\"\n");
+	printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");
+	return 0; // normal end of program
+}
